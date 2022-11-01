802ca65631addaf8a0f215ad2d4bec39bdfb04a2
Greensky <david.hesliere@gmail.com>
2022-11-01 19:14:51 +0100

    feat(ts): :sparkles: Add leave and station commands

---
+++ src/commands/station.ts
@@ -0,0 +1,38 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { LofiCommand } from "../structures/Command";
+import { stations } from '../utils/configs.json';
+
+export default new LofiCommand({
+    name: 'station',
+    description: "Changes the music station",
+    admin: true,
+    dm: false,
+    cooldown: 10,
+    options: [
+        {
+            name: 'station',
+            description: "Music station you want to switch",
+            required: true,
+            autocomplete: true,
+            type: ApplicationCommandOptionType.String
+        }
+    ],
+    execute: async({ interaction, options }) => {
+        const station = stations.find(x => x.url === options.getString('station'));
+        const queue = interaction.client.player.getQueue(interaction.guild);
+
+        if (!queue) return interaction.reply(`:x: | I'm not connected to a voice channel`).catch(() => {});
+        await interaction.deferReply();
+
+        const search = await interaction.client.player.search(station.url, {
+            requestedBy: interaction.user,
+        });
+
+        if (!search || search.tracks.length === 0) return interaction.editReply(`:x: | Music not found`).catch(() => {});
+        const found = `:white_check_mark: | Music found`;
+        await interaction.editReply(found);
+
+        await queue.play(search.tracks[0])
+        interaction.editReply(`${found}\n🎧 | Playing music\n${station.emoji} | Switched to ${station.name}`).catch(() => {});
+    }
+})
