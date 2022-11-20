b5eda23fe18657feee6a7329e86bed524da1e946
Greensky <david.hesliere@gmail.com>
2022-11-20 17:21:49 +0100

    fix(event): :bug: Fix command denietion

--- src/events/commandDenied.ts
+++ src/events/commandDenied.ts
@@ -5,8 +5,8 @@ export default new AmethystEvent('commandDenied', (command, reason) => {
     const includedReplies = [
         { x: commandDeniedCode.GuildOnly, y: "This command is not usable in direct messages" },
         { x: commandDeniedCode.UnderCooldown, y: `You have a cooldown of ${Math.floor((reason.metadata?.remainingCooldownTime ?? 1000) / 1000)} seconds on this command` },
-        { x: commandDeniedCode.UserMissingPerms, y: `You need ${reason.metadata.permissions.need.length} permissions to run this command. You have ${reason.metadata?.permissions?.got?.length}` },
-        { x: commandDeniedCode.ClientMissingPerms, y: `I need ${reason.metadata.permissions.need.length} permissions to run this command. I have ${reason.metadata.permissions.got.length}` }
+        { x: commandDeniedCode.UserMissingPerms, y: `You need ${reason.metadata.permissions?.need?.length} permissions to run this command. You have ${reason.metadata?.permissions?.got?.length}` },
+        { x: commandDeniedCode.ClientMissingPerms, y: `I need ${reason.metadata.permissions?.need?.length} permissions to run this command. I have ${reason.metadata.permissions?.got?.length}` }
     ]
     const fnt = command.interaction.replied ? 'editReply' : 'reply';
     if (includedReplies.find(x => x.x === reason.code)) {
