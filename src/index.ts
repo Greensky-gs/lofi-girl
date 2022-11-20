d9bc3f951df603d36722dc28374d44e107b2f3ba
Greensky <david.hesliere@gmail.com>
2022-11-20 15:18:27 +0100

    chore: :lock: Add a security to stations

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { AmethystClient } from "amethystjs"
 import { Player } from "discord-player";
 import { Partials, Client } from "discord.js"
 import { config } from "dotenv";
+import { checkForDuplicates } from "./utils/functions";
 
 config();
+
+const duplicated = checkForDuplicates();
+if (duplicated.length > 0) {
+    console.log(duplicated);
+    throw new Error("Some musics are duplicated");
+}
 
 export const client = new AmethystClient({
     intents: ['Guilds', 'GuildVoiceStates'],
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,17 @@
+import { station } from '../typings/station';
 import { stations } from './configs.json';
 
 export const getStationByUrl = (value: string) => {
     if (value === 'random') return stations[Math.floor(Math.random() * stations.length)];
     return stations.find(x => x.url === value);
+}
+export const checkForDuplicates = (): station[] => {
+    const tests: station[] = [];
+    const duplicated: station[] = [];
+
+    for (const st of stations as station[]) {
+        if (tests.includes(st)) duplicated.push(st);
+        else tests.push(st);
+    }
+    return duplicated;
 }
