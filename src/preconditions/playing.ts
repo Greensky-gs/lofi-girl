c0e7453d63a7f826a78c2c061850632f169b0251
Greensky <david.hesliere@gmail.com>
2022-11-20 15:54:02 +0100

    chore(precondition): :sparkles: Add playing precondition

---
+++ src/preconditions/playing.ts
@@ -0,0 +1,22 @@
+import { Precondition } from "amethystjs";
+
+module.exports = new Precondition('playing')
+.setChatInputRun(({ interaction }) => {
+    const queue = interaction.client.player.getQueue(interaction.guild);
+    if (!interaction.guild !|| interaction.guild.members.me?.voice?.channel || !queue || !queue.playing) {
+        return {
+            ok: false,
+            message: 'Not playing',
+            isChatInput: true,
+            interaction,
+            metadata: {
+                message: "I'm not playing music"
+            }
+        }
+    }
+    return {
+        ok: true,
+        isChatInput: true,
+        interaction
+    }
+})
