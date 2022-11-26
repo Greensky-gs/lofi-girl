fd8f2b7be9ad8a54d06885f3f94abd419154c041
Greensky <david.hesliere@gmail.com>
2022-11-26 13:21:47 +0100

    feat(cmd): :sparkles: Add leave command

---
+++ src/commands/leave.ts
@@ -0,0 +1,14 @@
+import { AmethystCommand, preconditions } from "amethystjs";
+import adminIfNotAlone from "../preconditions/adminIfNotAlone";
+import connected from "../preconditions/connected";
+import playing from "../preconditions/playing";
+
+export default new AmethystCommand({
+    name: 'leave',
+    description: "Leave the voice channel",
+    preconditions: [ preconditions.GuildOnly, connected, playing, adminIfNotAlone ]
+})
+.setChatInputRun(({ interaction }) => {
+    interaction.client.player.getQueue(interaction.guild).destroy(true);
+    interaction.reply(`🎧 | I left the voice channel`).catch(() => {});
+})
