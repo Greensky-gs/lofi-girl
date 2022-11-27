32f23529a6ae7e7fe1507fb6dddd4f07efc1ecf2
Greensky <david.hesliere@gmail.com>
2022-11-27 18:57:43 +0100

    feat: :contruction: Works on improve preconditions

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -1,12 +1,11 @@
 import { AmethystCommand, preconditions } from 'amethystjs';
 import adminIfNotAlone from '../preconditions/adminIfNotAlone';
 import connected from '../preconditions/connected';
-import playing from '../preconditions/playing';
 
 export default new AmethystCommand({
     name: 'leave',
     description: 'Leave the voice channel',
-    preconditions: [preconditions.GuildOnly, connected, playing, adminIfNotAlone]
+    preconditions: [preconditions.GuildOnly, connected, adminIfNotAlone]
 }).setChatInputRun(({ interaction }) => {
     interaction.client.player.getQueue(interaction.guild).destroy(true);
     interaction.reply(`🎧 | I left the voice channel`).catch(() => {});
