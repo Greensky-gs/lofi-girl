b7a006ec92eb9f132b145e5f31bdae41581ea2b3
Greensky <david.hesliere@gmail.com>
2022-11-30 14:07:33 +0100

    fix: :bug: Fix button denietion

--- src/events/buttonDenied.ts
+++ src/events/buttonDenied.ts
@@ -1,8 +1,8 @@
 import { AmethystEvent } from 'amethystjs';
 
 export default new AmethystEvent('buttonDenied', ({ button, metadata }) => {
-    const fnt = button[button.deferred || button.replied ? 'editReply' : 'reply'];
-    fnt({
+    const fnt = button.deferred || button.replied ? 'editReply' : 'reply';
+    button[fnt]({
         content: metadata.message ?? ":x: | You can't do this",
         ephemeral: true
     });
