119ffddaa4f85b29e8bd478c24a37dfd315eb3c6
Greensky <david.hesliere@gmail.com>
2022-11-30 13:38:26 +0100

    style: :children_crossing: Improve client presence data

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -26,20 +26,15 @@ export default new AmethystEvent('ready', async (client) => {
             };
         },
         async () => {
-            if (Object.keys(recommendation).length > 0) {
-                return {
-                    name: `${(recommendation as station).emoji} ${(recommendation as station).name}`,
-                    type: ActivityType.Listening,
-                    url: (recommendation as station).url
-                };
-            } else {
-                return {
-                    name: `music in ${client.player.queues.size} servers`,
-                    type: ActivityType.Playing
-                };
-            }
+            return {
+                name: `music in ${client.player.queues.size} servers`,
+                type: ActivityType.Playing
+            };
         }
     ];
+    const initialLength = statuses.length;
+    const actualRecommendation: station | {url: undefined} = {url: undefined};
+
     let index = 0;
     client.user.setActivity(await statuses[index]());
     index++;
@@ -47,5 +42,24 @@ export default new AmethystEvent('ready', async (client) => {
     setInterval(async () => {
         index++;
         client.user.setActivity(await statuses[index % statuses.length]());
+
+        if (Object.keys(recommendation).length > 0 && actualRecommendation?.url !== recommendation.url && actualRecommendation.url) {
+            const fnt = async(): Promise<ActivityOptions> => {
+                return {
+                    name: `${(recommendation as station).emoji} ${(recommendation as station).name}`,
+                    type: ActivityType.Listening,
+                    url: (recommendation as station).url
+                };
+            }
+            if (statuses.length > initialLength) {
+                statuses[4] = fnt;
+            } else {
+                statuses.push(fnt);
+            }
+        }
     }, 20000);
+
+    client.user.setPresence({
+        status: 'idle'
+    });
 });
