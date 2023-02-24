218d0a56b69cfa17ac40419df2e14799661e8c12
Greensky <david.hesliere@gmail.com>
2023-02-24 08:55:41 +0100

    chore: :label: Add feedbacks in stations

--- src/typings/station.ts
+++ src/typings/station.ts
@@ -1,8 +1,14 @@
 export type stationType = 'playlist' | 'station' | 'get a random station' | 'get the recommendation of the day';
 
+export type feedback = {
+    user_id: string;
+    keywords: string[];
+    comments: string;
+}
 export type station = {
     type: stationType;
     name: string;
     emoji: string;
     url: string;
+    feedbacks: feedback[];
 };
