7f1e30459948e5894006baf862d38caf16903caf
Greensky <david.hesliere@gmail.com>
2023-03-18 11:52:47 +0100

    build: :globe_with_meridians: Make internationalisation manager

---
+++ src/langs/imports.ts
@@ -0,0 +1,24 @@
+import frCommands from './texts/fr/commands.json';
+import frTexts from './texts/fr/texts.json';
+import frButtons from './texts/fr/buttons.json';
+import frSelectors from './texts/fr/selectors.json';
+
+import enCommands from './texts/en/commands.json';
+import enTexts from './texts/en/texts.json'
+import enButtons from './texts/en/buttons.json';
+import enSelectors from './texts/en/selectors.json';
+
+export default {
+    fr: {
+        commands: frCommands,
+        texts: frTexts,
+        buttons: frButtons,
+        selectors: frSelectors
+    },
+    en: {
+        commands: enCommands,
+        texts: enTexts,
+        buttons: enButtons,
+        selectors: enSelectors
+    }
+}
