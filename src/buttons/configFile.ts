1a0c75f4e61b77b7e38590af98f34bd3a024b4ac
Greensky <david.hesliere@gmail.com>
2023-03-23 16:22:13 +0100

    refactor: :recycle: Run prettier

--- src/buttons/configFile.ts
+++ src/buttons/configFile.ts
@@ -1,7 +1,7 @@
-import { ButtonHandler } from "amethystjs";
-import { PanelIds } from "../typings/bot";
-import botOwner from "../preconditions/botOwner";
-import { AttachmentBuilder } from "discord.js";
+import { ButtonHandler } from 'amethystjs';
+import { PanelIds } from '../typings/bot';
+import botOwner from '../preconditions/botOwner';
+import { AttachmentBuilder } from 'discord.js';
 
 export default new ButtonHandler({
     customId: PanelIds.ConfigFile,
@@ -9,21 +9,30 @@ export default new ButtonHandler({
 }).setRun(({ button }) => {
     const file = new AttachmentBuilder('./dist/utils/configs.json')
         .setName('configs.json')
-        .setDescription("Config file")
-    
-    if (!file) return button.reply({
-        content: "❌ | An error occured when fetching the config file",
-        ephemeral: true
-    }).catch(() => {});
+        .setDescription('Config file');
 
-    button.reply({
-        content: "Here is the config file",
-        files: [ file ],
-        ephemeral: true
-    }).catch((error) => {
-        button[button.replied ? 'editReply' : 'reply']({
-            ephemeral: true,
-            content: `An error occured while sending the file. This is probably because the file is too big to be sent.\n\`\`\`${JSON.stringify(error, null, 4)}\`\`\``
-        }).catch(() => {});
-    })
-})
+    if (!file)
+        return button
+            .reply({
+                content: '❌ | An error occured when fetching the config file',
+                ephemeral: true
+            })
+            .catch(() => {});
+
+    button
+        .reply({
+            content: 'Here is the config file',
+            files: [file],
+            ephemeral: true
+        })
+        .catch((error) => {
+            button[button.replied ? 'editReply' : 'reply']({
+                ephemeral: true,
+                content: `An error occured while sending the file. This is probably because the file is too big to be sent.\n\`\`\`${JSON.stringify(
+                    error,
+                    null,
+                    4
+                )}\`\`\``
+            }).catch(() => {});
+        });
+});
