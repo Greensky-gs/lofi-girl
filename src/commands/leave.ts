53604d7b065d84dc292a7f49f34140efad9252cd
Greensky <david.hesliere@gmail.com>
2023-04-05 15:51:15 +0200

    fix: :adhesive_bandage: Fix loop supression

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -2,6 +2,7 @@ import { AmethystCommand, preconditions } from 'amethystjs';
 import adminIfNotAlone from '../preconditions/adminIfNotAlone';
 import connected from '../preconditions/connected';
 import { buildLocalizations } from '../utils/functions';
+import { loops } from '../utils/maps';
 
 const locals = buildLocalizations('leave');
 export default new AmethystCommand({
@@ -12,5 +13,7 @@ export default new AmethystCommand({
     descriptionLocalizations: locals.description
 }).setChatInputRun(({ interaction }) => {
     interaction.client.player.nodes.get(interaction.guild).delete();
+    loops.delete(interaction.guild.id);
+
     interaction.reply(interaction.client.langs.getText(interaction, 'leave', 'reply')).catch(() => {});
 });
